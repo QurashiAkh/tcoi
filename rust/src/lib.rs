d2721143f7a5ee1d248b8868dc0858946cf99d52
QurashiAkh <hussain.ark@yahoo.com>
2023-02-10 16:12:52 +0300

    Added to the Rust API

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,12 +1,17 @@
 use rand::seq::IteratorRandom;
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    fs::{self, File, OpenOptions},
+    io::Write,
+    path::Path,
+};
 
 pub struct EmptyTextError;
 
 pub struct Hasher {}
 
 impl Hasher {
-    pub fn gen_tcoi(self) -> String {
+    pub fn gen_tcoi(&self) -> String {
         let mut rng = rand::thread_rng();
         let letters =
             String::from("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.");
@@ -19,11 +24,51 @@ impl Hasher {
         return code;
     }
 
-    pub fn get_tcoi(self, text: String) -> Result<HashMap<String, String>, EmptyTextError> {
+    fn check_hashfile(&self) {
+        // If .hashfile isn't available then create one
+
+        let hashfile_path = Path::new(".hashfile");
+
+        if !Path::exists(&hashfile_path) {
+            File::create(".hashfile").expect("UNABLE TO CREATE HASHFILE!");
+        }
+    }
+
+    pub fn get_tcoi(&self, text: String) -> Result<HashMap<String, String>, EmptyTextError> {
         if text.trim() == String::from("") {
             return Err(EmptyTextError);
         } else {
-            let code = self.gen_tcoi();
+            let mut code = String::new();
+
+            self.check_hashfile();
+
+            let hashfile_content =
+                fs::read_to_string(".hashfile").expect("UNABLE TO READ HASHFILE!");
+
+            for line in hashfile_content.split("\n") {
+                let found_vec = match line.rsplit_once(": ") {
+                    Some(found_vec) => found_vec,
+                    None => ("NONE FOUND!", "NONE FOUND!"),
+                };
+
+                if found_vec.1.starts_with("Q-") {
+                    if found_vec.0 == text {
+                        code = found_vec.1.to_string();
+                        break;
+                    }
+                }
+            }
+
+            if code == "" {
+                code = self.gen_tcoi();
+
+                let mut hashfile = OpenOptions::new()
+                    .append(true)
+                    .open(".hashfile")
+                    .expect("UNABLE TO OPEN HASHFILE!");
+
+                writeln!(hashfile, "{}: {}", text, code).expect("UNABLE TO WRITE INTO HASHFILE!");
+            }
 
             let hashmap: HashMap<String, String> = HashMap::from([(text, code)]);
 
